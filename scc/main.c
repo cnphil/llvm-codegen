789a475cd213cfd52bfcf67d2c63c290a01a4de1
Phil Xiaojun Hu <phil@cnphil.com>
2013-12-13 11:28:41 +0800

    Initial Commit

---
+++ scc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  scc
+//
+//  Created by Phil Hu on 13/12/13.
+//  Copyright (c) 2013 Phil Hu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
